bdc357a77639793e9ffc0cf9b40eeed737dbc2ff
SS <1530769634@qq.com>
2025-04-23 08:32:29 +0800

    ch5 sys_spawn()

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -163,7 +163,22 @@ pub fn sys_spawn(_path: *const u8) -> isize {
         "kernel:pid[{}] sys_spawn NOT IMPLEMENTED",
         current_task().unwrap().pid.0
     );
-    -1
+    let token = current_user_token();
+    let current_task = current_task().unwrap();
+    let new_task = current_task.fork();
+    let new_pid = new_task.pid.0;
+    let _path = translated_str(token, _path);
+    let trap_cx = new_task.inner_exclusive_access().get_trap_cx();
+    trap_cx.x[10] = 0;
+    if let Some(data) = get_app_data_by_name(_path.as_str()) {
+        new_task.exec(data);
+        let trap_cx = new_task.inner_exclusive_access().get_trap_cx();
+        trap_cx.x[10] = 0;
+        add_task(new_task);
+        new_pid as isize
+    } else {
+        -1
+    }
 }
 
 // YOUR JOB: Set task priority.
